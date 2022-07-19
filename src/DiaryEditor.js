9e51e3b7d1f448b6ea1d8a15a9bd24a57c8eb014
hjs0522 <91768822+hjs0522@users.noreply.github.com>
2022-07-19 23:41:36 +0900

    기본 골격 완성

---
+++ src/DiaryEditor.js
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+import { useState,useRef } from "react";
+
+const Editor = styled.div`
+border: 1px solid gray;
+text-align: center;
+padding: 20px;
+`
+
+const Input = styled.input`
+    margin-bottom: 20px;
+    width: 500px;
+    padding: 10px;
+`
+
+const Textarea = styled.textarea`
+    margin-bottom: 20px;
+    width: 500px;
+    padding:10px;
+    height:150px;
+`
+const Select = styled.select`
+    width: 300px;
+    padding: 10px;
+    margin-bottom: 20px;
+`
+
+const Button = styled.button`
+    width:500px;
+    padding: 10px;
+    cursor:pointer;
+`
+
+const DiaryEditor = () =>{
+    const [state,setState] = useState({
+        author:"",
+        content:"",
+        emotion:1
+    });
+    
+    const authorInput = useRef();
+    const contentInput = useRef();
+    
+    const handleChangeState = e =>{
+        setState({
+            ...state,
+            [e.target.name] : e.target.value
+        });
+    };
+    
+    const handleSubmit = () =>{
+        
+        if(state.author.length < 1){
+            authorInput.current.focus();
+            return;
+        }
+        
+        if(state.content.length < 5){
+            contentInput.current.focus();
+            return;
+        }
+        console.log(state);
+        alert("저장 성공!");
+    };
+    
+    return(
+        <Editor>
+            <h2>오늘의 일기</h2>
+            <div>
+                <Input
+                    ref = {authorInput}
+                    value={state.author}
+                    onChange={handleChangeState}
+                    name="author"
+                    placeholder="작성자"
+                    type="text">
+                </Input>
+            </div>
+            <div>
+                <Textarea
+                    ref = {contentInput}
+                    value={state.content}
+                    onChange={handleChangeState}
+                    name="content"
+                    placeholder="일기"
+                    type="text">
+                </Textarea>
+            </div>
+            <div>
+                <span>오늘의 감정점수 : </span>
+                <Select
+                    name="emotion"
+                    value={state.emotion}
+                    onChange={handleChangeState}
+                >
+                    <option value ={1}>1</option>
+                    <option value ={2}>2</option>
+                    <option value ={3}>3</option>
+                    <option value ={4}>4</option>
+                    <option value ={5}>5</option>
+                </Select>
+                    
+            </div>
+            <div>
+                <Button onClick={handleSubmit}>일기 저장하기</Button>
+            </div>
+        </Editor>
+    );
+};
+
+export default DiaryEditor;
